7e3a3a64efe8d9e21048521f7dbc83782f1596e0
intervinn <nikita.bondarenko335@gmail.com>
2025-08-31 19:31:59 +0300

    заполнил хэндлер оценок

--- Server/Handlers/GradeHandler.cs
+++ Server/Handlers/GradeHandler.cs
@@ -1,5 +1,7 @@
 ﻿
+using Microsoft.EntityFrameworkCore;
 using Server.Services;
+using Shared.Models;
 
 namespace Server.Handlers
 {
@@ -16,15 +18,45 @@ namespace Server.Handlers
 
         public async Task CreateAsync(object data) 
         {
-            
+            if (data is Grade g)
+            {
+                await _storage.Context.Grades.AddAsync(g);
+                await _storage.Context.SaveChangesAsync();
+            }
         }
         public async Task EditAsync(object data) 
         {
+            if (data is Grade g)
+            {
+                var obj = await _storage.Context.Grades.SingleAsync(p => p.Id == g.Id);
+                if (obj == null)
+                {
+                    throw new Exception("Обьект не найден");
+                }
 
+                var entry = _storage.Context.Entry(obj);
+                entry.CurrentValues.SetValues(g);
+
+                foreach (var property in entry.Properties)
+                {
+                    var original = entry.OriginalValues[property.Metadata.Name];
+                    var current = entry.CurrentValues[property.Metadata.Name];
+                    if (Equals(original, current))
+                    {
+                        property.IsModified = false;
+                    }
+                }
+
+                await _storage.Context.SaveChangesAsync();
+            }
         }
         public async Task DeleteAsync(object data) 
         {
-
+            if (data is Grade g)
+            {
+                _storage.Context.Grades.Remove(g);
+                await _storage.Context.SaveChangesAsync();
+            }
         }
     }
 }
