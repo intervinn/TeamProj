97c15e8f85f48cc4bc8a39eca6f4da5fcc02ea77
intervinn <nikita.bondarenko335@gmail.com>
2025-09-02 15:32:48 +0300

    дописал хэндлеры

--- Server/Handlers/ScheduleHandler.cs
+++ Server/Handlers/ScheduleHandler.cs
@@ -1,5 +1,7 @@
 ﻿
+using Microsoft.EntityFrameworkCore;
 using Server.Services;
+using Shared.Models;
 
 namespace Server.Handlers
 {
@@ -16,15 +18,45 @@ namespace Server.Handlers
 
         public async Task CreateAsync(object data)
         {
-
+            if (data is Schedule g)
+            {
+                await _storage.Context.Schedules.AddAsync(g);
+                await _storage.Context.SaveChangesAsync();
+            }
         }
         public async Task EditAsync(object data)
         {
+            if (data is Schedule g)
+            {
+                var obj = await _storage.Context.Schedules.SingleAsync(p => p.Id == g.Id);
+                if (obj == null)
+                {
+                    throw new Exception("Обьект не найден");
+                }
 
+                var entry = _storage.Context.Entry(obj);
+                entry.CurrentValues.SetValues(g);
+
+                foreach (var property in entry.Properties)
+                {
+                    var original = entry.OriginalValues[property.Metadata.Name];
+                    var current = entry.CurrentValues[property.Metadata.Name];
+                    if (Equals(original, current))
+                    {
+                        property.IsModified = false;
+                    }
+                }
+
+                await _storage.Context.SaveChangesAsync();
+            }
         }
         public async Task DeleteAsync(object data)
         {
-
+            if (data is Schedule g)
+            {
+                _storage.Context.Schedules.Remove(g);
+                await _storage.Context.SaveChangesAsync();
+            }
         }
     }
 }
--- Server/Handlers/TeacherHandler.cs
+++ Server/Handlers/TeacherHandler.cs
@@ -1,5 +1,7 @@
 ﻿
+using Microsoft.EntityFrameworkCore;
 using Server.Services;
+using Shared.Models;
 
 namespace Server.Handlers
 {
@@ -16,15 +18,45 @@ namespace Server.Handlers
 
         public async Task CreateAsync(object data)
         {
-
+            if (data is Teacher g)
+            {
+                await _storage.Context.Teachers.AddAsync(g);
+                await _storage.Context.SaveChangesAsync();
+            }
         }
         public async Task EditAsync(object data)
         {
+            if (data is Teacher g)
+            {
+                var obj = await _storage.Context.Teachers.SingleAsync(p => p.Id == g.Id);
+                if (obj == null)
+                {
+                    throw new Exception("Обьект не найден");
+                }
 
+                var entry = _storage.Context.Entry(obj);
+                entry.CurrentValues.SetValues(g);
+
+                foreach (var property in entry.Properties)
+                {
+                    var original = entry.OriginalValues[property.Metadata.Name];
+                    var current = entry.CurrentValues[property.Metadata.Name];
+                    if (Equals(original, current))
+                    {
+                        property.IsModified = false;
+                    }
+                }
+
+                await _storage.Context.SaveChangesAsync();
+            }
         }
         public async Task DeleteAsync(object data)
         {
-
+            if (data is Teacher g)
+            {
+                _storage.Context.Teachers.Remove(g);
+                await _storage.Context.SaveChangesAsync();
+            }
         }
     }
 }
